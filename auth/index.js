84bd8caa6232f2259996065091f6fc9ae0dcf7b9
Esdras Pavon <pavonesdras@gmail.com>
2020-03-09 17:44:51 -0300

    added auth middleware

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,47 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const secret = config.jwt.secret;
 
 function sign(data) {
-  return jwt.sign(data, 'secreto');
+  return jwt.sign(data, secret);
 }
 
+function verify(token){
+  return jwt.verify(token, secret)
+}
+
+const check = {
+  own: function(req, owner) {
+    const decoded = decodeHeader(req);
+    if(decoded.id !== owner) {
+      throw new Error('No tienes los permisos necesarios');
+    }
+  }
+}
+
+function getToken(auth) {
+  if(!auth) {
+    throw new Error('No hay token');
+  }
+  if(auth.indexOf('Bearer ') === -1) {
+    throw new Error('Formato de token invàlido');
+  }
+  let token = auth.replace('Bearer ', '');
+
+  return token;
+}
+
+function decodeHeader(req) {
+  const authorization = req.headers.authorization || '';
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded;
+
+  return decoded;
+}
 module.exports = {
   sign,
+  check,
 }
